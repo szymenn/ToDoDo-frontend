7c30551622529e3d1b434923e73a38e0c5008cbe
szymenn <ilikedzimi@gmail.com>
2020-01-08 14:16:02 +0100

    Updates

---
+++ precache-manifest.384d620a65cb05052f5b325485507e41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c561c259c6c1bfea1080c3d530900fa1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef2cb29d0a5881a80622",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ef2cb29d0a5881a80622",
+    "url": "/static/js/2.d9c42e81.chunk.js"
+  },
+  {
+    "revision": "a9fff99a02ab6c3d6326",
+    "url": "/static/js/main.d9bbfebe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
