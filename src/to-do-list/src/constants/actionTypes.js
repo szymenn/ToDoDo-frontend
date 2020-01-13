6c2bc62bd327911438688af297069dff951b6706
szymenn <ilikedzimi@gmail.com>
2020-01-13 13:01:13 +0100

    Updates

--- src/to-do-list/src/constants/actionTypes.js
+++
@@ -1,7 +0,0 @@
-export const ADD_TODO = "ADD_TODO";
-export const DELETE_TODO = "DELETE_TODO";
-export const UPDATE_TODOS = "UPDATE_TODOS";
-export const UPDATE_TODO = "UPDATE_TODO";
-export const SET_USERNAME = "SET_USERNAME";
-export const SET_UPDATE_ID = "SET_UPDATE_ID";
-export const SET_UP_ERROR = "SET_UP_ERROR";
--- src/to-do-list/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import reducer from '../reducers';
-import thunk from 'redux-thunk';
-
-const middleware = applyMiddleware(thunk)
-const store = createStore(reducer, middleware);
-
-export default store
