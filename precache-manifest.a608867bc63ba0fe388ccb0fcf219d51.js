4dca2bb7ba496317a7738726e08db44b27feed88
szymenn <ilikedzimi@gmail.com>
2020-01-14 17:27:13 +0100

    Updates

---
+++ precache-manifest.a608867bc63ba0fe388ccb0fcf219d51.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0a617e8cc17d381767e47eaa6826e1",
+    "url": "/ToDoDo-frontend/index.html"
+  },
+  {
+    "revision": "ef2cb29d0a5881a80622",
+    "url": "/ToDoDo-frontend/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "ef2cb29d0a5881a80622",
+    "url": "/ToDoDo-frontend/static/js/2.d9c42e81.chunk.js"
+  },
+  {
+    "revision": "f9bde8b268fc066cb2e4",
+    "url": "/ToDoDo-frontend/static/js/main.00e33d12.chunk.js"
+  },
+  {
+    "revision": "a97b2d16d4a35108648d",
+    "url": "/ToDoDo-frontend/static/js/runtime~main.3689b6b7.js"
+  }
+]);
--- src/constants/urls.js
+++
@@ -1,1 +0,0 @@
-export const apiUrl = 'https://to-do-do.herokuapp.com'
